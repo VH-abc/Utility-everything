1394ccec6b3651c532976b6b894d1649ee14216e
VH-abc <76539808+VH-abc@users.noreply.github.com>
2024-04-25 06:26:27 -0700

    Added more documentation

--- elo_optimizer.py
+++ elo_optimizer.py
@@ -1,12 +1,27 @@
+'''
+To use elo_optimizer.py from your own file:
+- Import it. It is a stateful module with globals:
+      ITEMS: list[Item]  
+      LOTTERIES: list[Lottery]  
+      RESULTS: list[Result]  
+      PARAMETERS: list[torch.Tensor]  
+      MODE = "compound" # "compound" or "overwrite" (mode for adding results for pairs that have already been played)  
+- Interact with it as follows:  
+    - Use the Item() and Lottery() constructors to create items and lotteries  
+    - Use add_result() to add results (don't use the Result constuctor directly)  
+    - Items, Lotteries, and Results have delete() methods. The delete() method removes an object, its dependents, and its parameters from global lists.
+    - Call optimization methods as needed to recompute the best fit utilities and temperatures. Currently only full_batch_optimize() exists.  
+    - Interact directly with the global lists for other operations  
+'''
 import numpy as np
 import torch
 from torch import stack, logsumexp
 from torch.nn.functional import log_softmax
 from dataclasses import dataclass
-from typing import Iterable
+from typing import Iterable, Callable
 
-def list_filter(f, lst):
-    '''Same as filter, but returns a list instead of an iterator.\n\nShorthand for [x for x in lst if f(x)].'''
+def list_filter(f:Callable, lst:Iterable) -> list:
+    '''Same as filter, but returns a list instead of filter object.\n\nShorthand for [x for x in lst if f(x)].'''
     return [x for x in lst if f(x)]
 
 class Item:
